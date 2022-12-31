a4b48ea16fbdcc4b7b2c2535b526d772c508cc16
DasLixou <einlixou@gmail.com>
2022-12-31 17:56:21 +0100

    Some playing :)

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ fn main() {
         .with(Padding::All(2))
         .with(Padding::Left(5))
         .with(Padding::Right(0));
-    let node = HStack::new()
-        .style(style.clone())
-        .children(Label::new("Hello, World!").style(style.clone()));
-    println!("{:?}", node);
+    let node = HStack::new().style(style).children((
+        Label::new("Hello, World!"),
+        Label::new("And hello again :)"),
+    ));
+    println!("{:#?}", node);
 }
